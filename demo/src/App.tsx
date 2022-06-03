c156ce49f738f385bead04ce31d69ebce13c4fb2
lutaok <lucadig.1994@gmail.com>
2022-06-03 10:23:40 +0200

    Updated demo

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -7,7 +7,7 @@ const App: Component = () => {
       <Stars style={{ color: 'blue' }} initialValue={3} />
       <Stars style={{ color: 'red', width: '50px' }} initialValue={1} disabled={true} />
       <Stars style={{ color: 'green', width: 100, height: '60px' }} initialValue={5} disabled={true} />
-      <Stars style={{ color: 'green', width: '200px', 'font-size': 23 }} initialValue={5} maxRating={20} />
+      <Stars style={{ color: 'green', width: '200px', 'font-size': 23 }} initialValue={5} maxRating={20} onClick={console.log} />
     </>
   );
 };
